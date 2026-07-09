97c994483024c8770e74faef8b932b1200630b63
devansha1233 <148952946+devansha1233@users.noreply.github.com>
2026-07-09 08:32:38 +0530

    Update API URL from v1beta to v1

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ export default function App() {
 
       // UPDATED URL: Swapped from v1beta to v1 and fixed the route for standard keys
       const response = await fetch(
-   `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+   `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash lite:generateContent?key=${GEMINI_API_KEY}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
